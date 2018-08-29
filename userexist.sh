205884868885d674df7fa6be4563d094087c07cf
localadmin <tejasgajjar@hotmail.com>
2018-08-29 14:25:05 -0500

    add

--- userexist.sh
+++ userexist.sh
@@ -3,4 +3,4 @@ echo " $(grep ^ec /etc/passwd)"
 echo " $(grep -E "[0-9\.\-]{2,}" /etc)"
 wget www.yelp.com 
 grep -c yelp index.html
-find / -mmin -60 # modification time
+find ~ -mmin -60 # modification time
