622f2756b306df6c385698008cd81316a7bb38ef
localadmin <tejasgajjar@hotmail.com>
2018-08-29 14:17:40 -0500

    error

--- userexist.sh
+++ userexist.sh
@@ -2,5 +2,5 @@
 echo " $(grep ^ec /etc/passwd)"
 echo " $(grep -E "[0-9\.\-]{2,}" /etc)"
 wget www.yelp.com 
-echo " $(grep -c yelp index.html)"
-echo " $(find / -mmin -60 \# modification time)"
+grep -c yelp index.html
+find / -mmin -60 # modification time
