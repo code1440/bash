94f06a29fe8adbc38988e68ec8304b0065835f4b
localadmin <tejasgajjar@hotmail.com>
2018-08-29 11:41:20 -0500

    changed commands

--- print_host_ip.sh
+++ print_host_ip.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-hostname
-/sbin/ifconfig eth0
-uname -a
-TOP 
-Cat >> report_8292018
+file_name=report_8292018
+echo "Free Mem = $(free -m)\n" >> file_name
+echo "$(hostname)" >> file_name
+echo "$(ifconfig eth0)" >> file_name
